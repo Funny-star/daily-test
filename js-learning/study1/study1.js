21f3837e2d32feddfa180219bbdc1f25884acf6f
Funny-star <1185012006@qq.com>
2025-11-26 22:15:12 +0800

    study1

---
+++ js-learning/study1/study1.js
@@ -0,0 +1,50 @@
+// 输出hello world
+// alert ("hello world")
+// document.writeln("hello world")
+// console.log("hello world")
+
+// 变量的类型检测
+// var a = 10
+// document.writeln(typeof a) // number
+
+// 三元运算符
+// 1. 条件 ? 表达式1 : 表达式2
+// var a = 2
+// a > 5 ? document.writeln("大于5") : document.writeln("小于或等于5")
+// 2. 变量 = 条件 ? 表达式1 : 表达式2
+// var a = 10
+// var b = a > 5 ? "大于5" : "小于或等于5"
+// document.writeln(b)
+
+// 三元运算练习(嵌套)
+// 满200减10，满100减5
+// var a = 300
+// var b = a>=200 ? b=a-10 : (a >= 100? b = a - 5 : b = a)
+// document.writeln(b)
+
+// if语句
+// var a = 100,b
+// if(a >= 200){
+//     b = a - 10
+//     document.writeln(b)
+// }
+// else if(200 > a && a >= 100){
+//     b = a - 5
+//     document.writeln(b)
+// }
+// else{
+//     document.writeln(a)
+// }
+
+// if-test
+// var year = 2016
+// if(year % 400 === 0 || (year % 4 === 0 && year % 100 != 0)){ //通过加小括号来保证代码优先级，以此使得逻辑更加通顺
+//     document.writeln("是润年")
+
+// }                               
+// else{
+//     document.writeln("不是润年")
+// }
+
+// 优先级判断
+// document.writeln(1 || 3 && 2)  数字代表优先级（数字小的优先级高）
