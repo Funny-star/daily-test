956c84304d5a866c6df6180527b1d7c1ccc3b9a1
Funny-star <1185012006@qq.com>
2025-12-01 16:51:03 +0800

    study2

--- js-learning/study1/study1.js
+++ js-learning/study1/study1.js
@@ -122,3 +122,152 @@
 //         break
 // }
 
+// 循环分支语句
+
+// while 语句
+// var rum = 0
+// while (rum < 10){
+//     document.writeln("当前的rum的值" + rum)
+//     rum++
+// }
+
+// dowhile 语句
+
+// var rum = 0
+// do {
+//     document.writeln("hello"+rum)
+//     rum++
+// } while(rum < 10)
+
+// var rum = 1
+// var result = 0
+// do{
+//     result += rum
+//     rum++
+// } while(rum <= 100)
+// document.writeln(result)
+
+// for循环
+//九九乘法表
+// for (var a = 1; a <= 9; a++) {
+//     for (var b = 1; b <= a; b++) {
+//         var result = a * b;
+//         document.writeln("<span>"+b+ "x" +a+"="+result+"</span>");
+//     }
+//     document.writeln("<br>");
+// }
+
+
+// 函数
+// function test(n){
+//     if(n === undefined){
+//         alert("请传入参数")
+//     }
+//     for (var a = 1; a <= n; a++) {
+//     for (var b = 1; b <= a; b++) {
+//         var result = a * b;
+//         document.writeln("<span>"+b+ "x" +a+"="+result+"</span>");
+//     }
+//     document.writeln("<br>");
+// }
+// }
+
+// test(8)
+// test()
+
+// 对象
+// 创建对象
+// let obj1 = new Object();
+// obj1.name = "wxy";
+// obj1.age = 19;
+// console.log(obj1); // {name: "wxy", age: 19}
+
+// // 增 (Add)
+// obj1.gender = "male";           // 添加新属性
+// obj1["email"] = "wxy@example.com"; // 另一种添加方式
+// console.log(obj1); // {name: "wxy", age: 19, gender: "male", email: "wxy@example.com"}
+
+// // 删 (Delete)
+// delete obj1.age;                // 删除属性
+// delete obj1["email"];          // 另一种删除方式
+// console.log(obj1); // {name: "wxy", gender: "male"}
+
+// // 改 (Update)
+// obj1.name = "newName";         // 修改属性值
+// obj1["gender"] = "female";     // 另一种修改方式
+// console.log(obj1); // {name: "newName", gender: "female"}
+
+// // 查 (Query)
+// console.log(obj1.name);        // 访问属性值 - "newName"
+// console.log(obj1["gender"]);   // 另一种访问方式 - "female"
+// console.log("name" in obj1);   // 检查属性是否存在 - true
+// console.log(obj1.hasOwnProperty("name")); // 检查自身属性 - true
+
+// 创建对象
+// let obj2 = {
+//     name: "wxy",
+//     age: 19
+// };
+// console.log(obj2); // {name: "wxy", age: 19}
+
+// // 增 (Add)
+// obj2.gender = "male";
+// obj2["hobby"] = "coding";
+// console.log(obj2); // {name: "wxy", age: 19, gender: "male", hobby: "coding"}
+
+// // 删 (Delete)
+// delete obj2.age;
+// delete obj2["hobby"];
+// console.log(obj2); // {name: "wxy", gender: "male"}
+
+// // 改 (Update)
+// obj2.name = "updatedName";
+// obj2["gender"] = "female";
+// console.log(obj2); // {name: "updatedName", gender: "female"}
+
+// // 查 (Query)
+// console.log(obj2.name);        // "updatedName"
+// console.log(obj2["gender"]);   // "female"
+// console.log("name" in obj2);   // true
+// console.log(obj2.age);         // undefined (属性不存在)
+// console.log(Object.keys(obj2)); // 获取所有属性名: ["name", "gender"]
+// console.log(Object.values(obj2)); // 获取所有属性值: ["updatedName", "female"]
+
+// // 创建构造函数
+// function Person() {
+//     this.name = "wxy";
+//     this.age = 19;
+// }
+
+// // 创建对象
+// let obj3 = new Person();
+// console.log(obj3); // Person {name: "wxy", age: 19}
+
+// // 增 (Add)
+// obj3.gender = "male";
+// obj3["country"] = "China";
+// console.log(obj3); // Person {name: "wxy", age: 19, gender: "male", country: "China"}
+
+// // 删 (Delete)
+// delete obj3.age;
+// delete obj3["country"];
+// console.log(obj3); // Person {name: "wxy", gender: "male"}
+
+// // 改 (Update)
+// obj3.name = "modifiedName";
+// obj3["gender"] = "female";
+// console.log(obj3); // Person {name: "modifiedName", gender: "female"}
+
+// // 查 (Query)
+// console.log(obj3.name);        // "modifiedName"
+// console.log(obj3["gender"]);   // "female"
+// console.log(obj3 instanceof Person); // true (检查对象类型)
+// console.log(Object.getOwnPropertyNames(obj3)); // 获取自身属性: ["name", "gender"]
+
+// // 遍历属性
+// for (let key in obj3) {
+//     if (obj3.hasOwnProperty(key)) {
+//         console.log(`${key}: ${obj3[key]}`);
+//     }
+// }
+
